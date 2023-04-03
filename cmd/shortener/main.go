45024f44a3db941109de80bc98b344183000611c
Alexander <kaun-aleksandr@yandex.ru>
2023-04-03 09:43:49 +0500

    increment 1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,47 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func mainHandle(w http.ResponseWriter, r *http.Request) {
+	d := strings.TrimPrefix(r.URL.Path, "/")
+
+	if d == "" {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+			return
+		}
+		if r.Header.Get("Content-Type") != "text/plain" {
+			http.Error(w, "Invalid Content-Type!", http.StatusBadRequest)
+			return
+		}
+
+		responseData, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Print("Получено значение: ", string(responseData))
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("http://localhost:8080/EwHXdJfB"))
+		return
+	}
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Add("Location", "https://practicum.yandex.ru/")
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", mainHandle)
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
