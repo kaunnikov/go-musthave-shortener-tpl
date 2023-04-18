b62efdb640e10f9cc1cbdb87c7eb6c158c8f2716
Alexander <kaun-aleksandr@yandex.ru>
2023-04-18 11:03:33 +0500

    debug

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -11,70 +11,11 @@ import (
 	"testing"
 )
 
-//func Test_mainHandle(t *testing.T) {
-//	type want struct {
-//		statusCode int
-//	}
-//	tests := []struct {
-//		name    string
-//		method  string
-//		request string
-//		body    string
-//		want    want
-//	}{
-//		{
-//			name:    "GET запрос к методу, который принимает только POST",
-//			request: "/",
-//			method:  http.MethodGet,
-//			want:    want{http.StatusBadRequest},
-//		},
-//		{
-//			name:    "POST запрос без тела",
-//			request: "/",
-//			method:  http.MethodPost,
-//			want:    want{http.StatusBadRequest},
-//		},
-//		{
-//			name:    "Хороший POST запрос",
-//			request: "/",
-//			method:  http.MethodPost,
-//			body:    "https://yandex.ru",
-//			want:    want{http.StatusCreated},
-//		},
-//		{
-//			name:    "POST запрос к методу, который принимает только GET",
-//			request: "/fdaw43",
-//			method:  http.MethodPost,
-//			want:    want{http.StatusBadRequest},
-//		},
-//		{
-//			name:    "GET запрос к методу с несуществующим id",
-//			request: "/not-allowed-id",
-//			method:  http.MethodGet,
-//			want:    want{http.StatusBadRequest},
-//		},
-//	}
-//
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//
-//			body := strings.NewReader(tt.body)
-//			request := httptest.NewRequest(tt.method, tt.request, body)
-//			w := httptest.NewRecorder()
-//			mainHandle(w, request)
-//			res := w.Result()
-//			assert.Equal(t, res.StatusCode, tt.want.statusCode)
-//			defer res.Body.Close()
-//		})
-//	}
-//}
-
-var testkeymap = map[string]string{}
-var fullUrl string
-var shortUrl string
+var fullURL string
+var shortURL string
 
 func testRequest(t *testing.T, ts *httptest.Server, method, path string) (int, string) {
-	body := strings.NewReader(fullUrl)
+	body := strings.NewReader(fullURL)
 	req, err := http.NewRequest(method, ts.URL+path, body)
 	require.NoError(t, err)
 	resp, err := http.DefaultClient.Do(req)
@@ -94,7 +35,7 @@ func testRequest(t *testing.T, ts *httptest.Server, method, path string) (int, s
 }
 
 func TestRouter(t *testing.T) {
-	fullUrl = "https://yandex.ru"
+	fullURL = "https://yandex.ru"
 	r := Router()
 	ts := httptest.NewServer(r)
 	defer ts.Close()
@@ -102,11 +43,11 @@ func TestRouter(t *testing.T) {
 
 	pat := regexp.MustCompile(`:\d{2,}/(\w+)`)
 	if len(pat.FindSubmatch([]byte(body))) == 2 {
-		shortUrl = "/" + string(pat.FindSubmatch([]byte(body))[1])
+		shortURL = "/" + string(pat.FindSubmatch([]byte(body))[1])
 	}
 
 	assert.Equal(t, http.StatusCreated, statusCode)
-	statusCode, _ = testRequest(t, ts, "GET", shortUrl)
+	statusCode, _ = testRequest(t, ts, "GET", shortURL)
 
 	assert.Equal(t, http.StatusOK, statusCode)
 
