34167a4c5e36c756c6e4dabe7519d8ea333c1a4c
Alexander <kaun-aleksandr@yandex.ru>
2023-05-24 13:08:31 +0500

    increment 10

--- internal/app/ping.go
+++ internal/app/ping.go
@@ -1,13 +1,18 @@
 package app
 
 import (
+	"context"
 	"database/sql"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"kaunnikov/go-musthave-shortener-tpl/internal/logging"
 	"net/http"
+	"time"
 )
 
 func (m *app) PingHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(r.Context(), 3*time.Second)
+	defer cancel()
+
 	db, err := sql.Open("pgx", m.cfg.DatabaseDSN)
 	if err != nil {
 		logging.Errorf("DB don't open: %s", err)
@@ -15,7 +20,7 @@ func (m *app) PingHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	_, err = db.Exec("SELECT * FROM information_schema.tables")
+	_, err = db.QueryContext(ctx, "SELECT * FROM information_schema.tables")
 	if err != nil {
 		logging.Errorf("DB don't work: %s", err)
 		w.WriteHeader(http.StatusInternalServerError)
